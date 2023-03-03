74c68473e73846193434134905c7098bb0d3853f
aymangafoorn <ayman@tridz.com>
2023-03-03 11:53:50 +0530

    title name change

--- frontend/src/components/global/page/Title.jsx
+++ frontend/src/components/global/page/Title.jsx
@@ -18,7 +18,6 @@ function Title(props) {
           {props.title}   
         </button>
 
-
       {/* {props.button_text && (
         <button
           className="relative inline-flex h-9 items-center rounded-md border border-transparent bg-slate-900 px-4 py-2 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
